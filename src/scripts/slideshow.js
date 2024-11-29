bdad2bb33c300515dfe8ffd92e3c82ed614866f1
devrafael-santos <raafael.cs@gmail.com>
2024-11-29 12:46:58 -0300

    first commit

---
+++ src/scripts/slideshow.js
@@ -0,0 +1,67 @@
+let slideIndex = 1;
+
+let slides = document.getElementsByClassName("mySlides");
+
+if (this.screen.width <= 890) {
+    for (i = 0; i < slides.length; i++) {
+        console.log(this.screen.width);
+        slides[i].style = "none";
+    }
+} else {
+    showSlides(slideIndex);
+}
+
+
+window.addEventListener("resize", function () {
+    if (this.screen.width <= 890) {
+        for (i = 0; i < slides.length; i++) {
+            console.log(this.screen.width);
+            slides[i].style = "none";
+        }
+    } else {
+        showSlides(slideIndex);
+    }
+});
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+
+    let i;
+    let dots = document.getElementsByClassName("dots");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].src = "/src/img/icons/dot-off.png";
+    }
+
+    slides[slideIndex - 1].style.display = "grid";
+    dots[slideIndex - 1].src = "/src/img/icons/dot-on.png";
+
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    var hamburger = document.getElementById("hamburger");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        hamburger.style.marginTop = "0px";
+    } else {
+        x.className = "topnav";
+        hamburger.style.marginTop = "72px";
+    }
+}
